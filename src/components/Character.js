3308713237f0394f2b902fcf7baeb453f874739e
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-11 15:21:30 +0600

    loading replaced with spinner

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CharCard from './CharCard';
-
+import spin from '../img/spin.gif';
 const Character = (props) => {
     const loading = props.isLoading
     const data = props.data
@@ -9,7 +9,7 @@ const Character = (props) => {
         <div className="container-fluid">
             <div className="row">
                 {
-                    loading ? <div><h1 className="bg-dark text-primary text-center">Loading.....</h1></div> :
+                    loading ? <div className="text-center"><img src={spin} className="w-100" alt="" /></div> :
                         data.map(item => <div className="col-md-4 py-2">
                             <CharCard key={item.char_id} charData={item}></CharCard>
                         </div>)
